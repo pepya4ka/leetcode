2f1740283edb7f1b4e5685eb345065aee21253ac
pepya4ka <ndrozdov9@gmail.com>
2023-03-11 23:10:07 +0300

    add solve for integer-to-roman task (https://leetcode.com/problems/integer-to-roman/)

--- src/main/kotlin/com/pepyachka/medium/integer_to_roman/Solution.kt
+++ src/main/kotlin/com/pepyachka/medium/integer_to_roman/Solution.kt
@@ -1,4 +1,14 @@
 package com.pepyachka.medium.integer_to_roman
 
 class Solution {
+    fun intToRoman(num: Int): String = intToRoman(num, "")
+
+    private fun intToRoman(num: Int, res: String): String {
+        if (num == 0) {
+            return res
+        }
+
+        val roman = Roman.values().reversedArray().first { r -> num >= r.value }
+        return intToRoman(num - roman.value, res + roman.name)
+    }
 }
