b9bcb14d26c09aa92cb13ab35c451262df5c0431
pepya4ka <ndrozdov9@gmail.com>
2023-03-24 20:19:21 +0300

    refact minimum-subsequence-in-non-increasing-order task (https://leetcode.com/problems/minimum-subsequence-in-non-increasing-order)

--- src/main/kotlin/com/pepyachka/easy/minimum_subsequence_in_non_increasing_order/Solution.kt
+++ src/main/kotlin/com/pepyachka/easy/minimum_subsequence_in_non_increasing_order/Solution.kt
@@ -3,10 +3,14 @@ package com.pepyachka.easy.minimum_subsequence_in_non_increasing_order
 class Solution {
     fun minSubsequence(nums: IntArray): List<Int> {
         val res = mutableListOf<Int>()
+        var sumArray = nums.sum()
+        var sumRes = 0
         nums.sortDescending()
         var i = 0
-        while (res.sum() <= nums.sum()) {
+        while (sumRes <= sumArray) {
             res.add(nums[i])
+            sumRes += nums[i]
+            sumArray -= nums[i]
             nums[i] = 0
             i++
         }
